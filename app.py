27ced060dbed5c7932068fde370360980a8bb63b
caochk <chenkai@outlook.it>
2021-07-30 12:08:49 +0800

    Add minimal home page

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
